d0c2ece4c57b8039168ae529a3795526c922c6bc
sherzod555 <wiut00007584@gmail.com>
2024-01-17 18:33:30 +0500

    bug fixed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link } from "react-scroll";
 
 import Banner from "../../assets/banner.jpg"
 import Download from "../../assets/download.svg"
