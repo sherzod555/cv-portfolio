bca0eccfb2c81bd657e3eda7c69b0c8898d56989
sherzod555 <wiut00007584@gmail.com>
2024-01-25 20:47:20 +0500

    header icons bug fixed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -50,13 +50,13 @@ export const Header = () => {
                         {/* Page settings */}
                         <div className="flex items-center gap-x-8">
                             <div className="flex items-center gap-x-2 rounded-2xl border hover:scale-90 duration-300 px-3 py-2">
-                                <img className="h-7 w-auto" src={Download} alt="resume" />
+                                <img className={`${isDarkMode? "invert" : ""} h-7 w-auto`} src={Download} alt="resume" />
 
                                 <a href="../../Resume.docx" download="Resume-Sherzod-Yodgorov.docx">Resume</a>
                             </div>
 
                             <div onClick={toggleTheme} className="flex items-center rounded-2xl border p-2 cursor-pointer">
-                                <img className="h-7 w-auto" src={isDarkMode ? Sun : Moon} alt={isDarkMode ? "Light Mode" : "Dark Mode"} />
+                                <img className={`${isDarkMode? "invert" : ""} h-7 w-auto`} src={isDarkMode ? Sun : Moon} alt={isDarkMode ? "Light Mode" : "Dark Mode"} />
                             </div>
 
 
