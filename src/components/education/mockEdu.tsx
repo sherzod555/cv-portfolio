6fa796dc2ea2ae93fb05dac6bbd74d271b45fa8f
sherzod555 <wiut00007584@gmail.com>
2024-04-12 19:16:13 +0500

    edu mock data changed

--- src/components/education/mockEdu.tsx
+++ src/components/education/mockEdu.tsx
@@ -1,84 +1,108 @@
 export const mockDataEdu = [
-    {
-      UniverName_eng: "The state secondary school specialised in foreign languages under the Ministry of Public education",
-      UniverName_uzb: "Xalq ta'lim vazirligi tasarrufidagi xorijiy tillarga ixtisoslashtirilgan davlat umumtaʼlim maktabi",
-      UniverName_rus: "Государственная общеобразовательная школа, специализирующаяся на иностранных языках при Министерстве народного образования",
-      FacultyName:"",
-      date: "2010-2015",
-    },
-    {
-      UniverName_eng: "Academic Lyceum of the International Islamic Academy of Uzbekistan",
-      FacultyName_eng: "Exact Sciences",
-      
-      UniverName_uzb: "O'zbekiston xalqaro islom akademiyasi akademik litseyi",
-      FacultyName_uzb: "Aniq fanlar",
-      
-      UniverName_rus: "Академический лицей Международной исламской академии Узбекистана",
-      FacultyName_rus: "Точные науки",
 
-      date: "2015-2018",
-    },
-    {
-      UniverName_eng: "Course in Pre-Foundation Studies at WIUT",
-      FacultyName_eng: "Basics of English for Academic Purposes",
-      
-      UniverName_uzb: "TXVUga kirish uchun tayyorgarlik kursi",
-      FacultyName_uzb: "Akademik maqsadlar uchun ingliz tili asoslari",
-      
-      UniverName_rus: "Курс подготовки к поступлению в МВУТ",
-      FacultyName_rus: "Основы английского языка для академических целей",
-      
-      date: "2016-2018",
-    },
-    {
-      UniverName_eng: "Course in Pre-Foundation Studies at WIUT",
-      FacultyName_eng: "Basics of Quantitative Skills",
-      
-      UniverName_uzb: "TXVUga kirish uchun tayyorgarlik kursi",
-      FacultyName_uzb: "Miqdoriy ko'nikmalar asoslari",
-      
-      UniverName_rus: "Курс подготовки к поступлению в МВУТ",
-      FacultyName_rus: "Основы количественных навыков",
-      
-      date: "2017-2018",
-    },
-    {
-      UniverName_eng: "Westminster International University in Tashkent",
-      FacultyName_eng: "Certificate in International Foundation Studies (CIFS)",
-      
-      UniverName_uzb: "Toshkentdagi Xalqaro Vestminster universiteti",
-      FacultyName_uzb: "Xalqaro fundamental ta'lim sertifikati (XFTS)",
-      
-      UniverName_rus: "Международный Вестминстерский Университет в Ташкенте",
-      FacultyName_rus: "Сертификат международного фундаментального обучения (СМФО)",
-      
-      date: "2018-2019",
-    },
-    {
-      UniverName_eng: "Westminster International University in Tashkent",
-      FacultyName_eng: "Business Information Systems",
-      
-      UniverName_uzb: "Toshkentdagi Xalqaro Vestminster universiteti",
-      FacultyName_uzb: "Biznes axborot tizimlari",
-      
-      UniverName_rus: "Международный Вестминстерский Университет в Ташкенте",
-      FacultyName_rus: "Бизнес-информационные системы",
+  {
+    UniverName: "Bobir Akilkhanov tech academy",
+    FacultyName: "CGI - Motion Design",
+    date_eng: "2024",
+    date_uzb: "2024",
+    date_rus: "2024",
+  },
 
-      date: "2019-2024",
-    },
-    {
-      UniverName: "Najot Ta'lim",
-      FacultyName: "Frontend ReactJS (Standard)",
-      date: "2022-2023",
-    },
-    {
-      UniverName: "Bobir Akilkhanov tech academy",
-      FacultyName: "CGI - Motion Design",      
-      date_eng: "2024-Present",
-      date_uzb: "2024-Hozirgacha",
-      date_rus: "2024-Настоящее время",
+  {
+    UniverName: "Najot Ta'lim",
+    FacultyName: "Frontend React JS (Standard)",
+    date: "2022-2023",
+  },
 
-    },
-  ];
+  {
+    UniverName_eng: "Westminster International University in Tashkent",
+    FacultyName_eng: "Business Information Systems",
 
-  export default mockDataEdu;
+    UniverName_uzb: "Toshkentdagi Xalqaro Vestminster universiteti",
+    FacultyName_uzb: "Biznes axborot tizimlari",
+
+    UniverName_rus: "Международный Вестминстерский Университет в Ташкенте",
+    FacultyName_rus: "Бизнес-информационные системы",
+
+    date: "2019-2024",
+  },
+
+  {
+    UniverName_eng: "Westminster International University in Tashkent",
+    FacultyName_eng: "Certificate in International Foundation Studies (CIFS)",
+
+    UniverName_uzb: "Toshkentdagi Xalqaro Vestminster universiteti",
+    FacultyName_uzb: "Xalqaro fundamental ta'lim sertifikati (XFTS)",
+
+    UniverName_rus: "Международный Вестминстерский Университет в Ташкенте",
+    FacultyName_rus:
+      "Сертификат международного фундаментального обучения (СМФО)",
+
+    date: "2018-2019",
+  },
+
+  {
+    UniverName_eng: "Course in Pre-Foundation Studies at WIUT",
+    FacultyName_eng: "Basics of Quantitative Skills",
+
+    UniverName_uzb: "TXVUga kirish uchun tayyorgarlik kursi",
+    FacultyName_uzb: "Miqdoriy ko'nikmalar asoslari",
+
+    UniverName_rus: "Курс подготовки к поступлению в МВУТ",
+    FacultyName_rus: "Основы количественных навыков",
+
+    date: "2017-2018",
+  },
+
+  {
+    UniverName_eng: "Course in Pre-Foundation Studies at WIUT",
+    FacultyName_eng: "Basics of English for Academic Purposes",
+
+    UniverName_uzb: "TXVUga kirish uchun tayyorgarlik kursi",
+    FacultyName_uzb: "Akademik maqsadlar uchun ingliz tili asoslari",
+
+    UniverName_rus: "Курс подготовки к поступлению в МВУТ",
+    FacultyName_rus: "Основы английского языка для академических целей",
+
+    date: "2016-2018",
+  },
+
+  {
+    UniverName_eng:
+      "Academic Lyceum of the International Islamic Academy of Uzbekistan",
+    FacultyName_eng: "Exact Sciences",
+
+    UniverName_uzb: "O'zbekiston xalqaro islom akademiyasi akademik litseyi",
+    FacultyName_uzb: "Aniq fanlar",
+
+    UniverName_rus:
+      "Академический лицей Международной исламской академии Узбекистана",
+    FacultyName_rus: "Точные науки",
+
+    date: "2015-2018",
+  },
+
+  {
+    UniverName_eng:
+      "The state secondary school specialized in foreign languages under the Ministry of Public education",
+    UniverName_uzb:
+      "Xalq ta'limi vazirligi tasarrufidagi xorijiy tillarga ixtisoslashtirilgan davlat umumta'lim maktabi",
+    UniverName_rus:
+      "Государственная общеобразовательная школа, специализирующаяся на иностранных языках при Министерстве народного образования",
+    FacultyName: "",
+    date: "2010-2015",
+  },
+
+  {
+    UniverName_eng:
+      "'KAMOL AVLOD' pre-school and primary educational complex",
+    UniverName_uzb:
+      "'KAMOL AVLOD' nomli maktabgacha va boshlang'ich ta'lim majmuasi",
+    UniverName_rus:
+      "'КАМОЛ АВЛОД' дошкольно-начальный образовательный комплекс",
+    FacultyName: "",
+    date: "2006-2010",
+  },
+];
+
+export default mockDataEdu;
