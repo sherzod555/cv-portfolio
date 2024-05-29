bacaa67cf3d3abd4ad1fabaa138b75ec435875a6
sherzod555 <wiut00007584@gmail.com>
2024-05-30 01:16:41 +0500

    I'm back. just did a lil work on Portfolio

--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,21 +1,28 @@
-import { useTheme } from '../../ThemeContext.tsx';
+import { useTheme } from "../../ThemeContext.tsx";
+import { useLanguage } from '../../LanguageContext.tsx';
+
 export const Portfolio = () => {
-    const { isDarkMode } = useTheme();    
+  const { isDarkMode } = useTheme();
+  const { language } = useLanguage();
 
-    return (
-        <>
-            <main className={`${isDarkMode ? "bg-slate-700 text-white" : "bg-blue-100"}`} id="portfolio">
 
-                <div className="container mx-auto xl:w-[1200px] px-7">
+  return (
+    <>
+      <main
+        className={`${isDarkMode ? "bg-slate-700 text-white" : "bg-blue-100"}`}
+        id="portfolio"
+      >
+        <div className="container mx-auto xl:w-[1200px] px-7">
+          <div className="py-8">
+          <h2 className="text-3xl font-bold">{`${language === "en" ? "Portfolio" : language === "uz" ? "Portfolio" : "Портфолио"}`}</h2>
 
-                    <div className="py-8">
-                        <h2 className="text-3xl font-bold">Portfolio</h2>
-                    </div>
 
-                </div>
-            </main>
-        </>
-    );
+            
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
-export default Portfolio
+export default Portfolio;
--- src/components/profile/mockdataProfile.tsx
+++ src/components/profile/mockdataProfile.tsx
@@ -6,34 +6,20 @@ export const mockDataProfile = [
     },
     {
         Ability_eng: "Problem-Solving Skills: Developed strong analytical and problem-solving skills through various coding challenges and real-world application of programming concepts. Excels at troubleshooting and finding creative solutions.",
-        Ability_uzb: "prob ab uzb",
+        Ability_uzb: "Muammolarni hal qilish ko'nikmalari: Turli xil kodlash muammolari va dasturlash tushunchalarini amaliy qo'llash orqali rivojlangan kuchli analitik va muammolarni hal qilish qobiliyati. Muammolarni bartaraf etish va ijodiy yechimlarni topishda juda yaxshi ko'nikmaga ega.",
         Ability_rus: "Навыки решения проблем: развитые сильные аналитические навыки и навыки решения проблем посредством различных задач кодирования и практического применения концепций программирования. Отлично справляется с устранением неполадок и поиском творческих решений."
     },
     {
         Ability_eng: "Adaptability and Eagerness to Learn: Ability to quickly grasp new technologies and programming languages. Demonstrated commitment to continuous learning, staying informed about the latest developments in the IT field.",
-        Ability_uzb: "adap ab uzb",
+        Ability_uzb: "Moslashuvchanlik va o'rganishga intilish: Yangi texnologiyalar va dasturlash tillarini tezda o'zlashtirishda qobiliyatli. IT sohasidagi so'nggi ishlanmalardan xabardor bo'lish va uzluksiz o'rganishga sodiqlikni namoyish etadi.",
         Ability_rus: "Адаптивность и стремление учиться: способность быстро осваивать новые технологии и языки программирования. Продемонстрированная приверженность непрерывному обучению и постоянному информированию о последних событиях в сфере ИТ."
     },
     {
         Ability_eng: "Communication Skills: Possesses excellent verbal and written communication skills, cultivated through collaborative coursework and group projects. Able to effectively convey technical concepts to both technical and non-technical audiences.",
-        Ability_uzb: "commun ab uzb",
+        Ability_uzb: "Muloqot ko'nikmalari: Jamoaviy kurs ishlari va guruh loyihalari orqali rivojlangan og'zaki va yozma muloqot qobiliyatlariga ega. Texnik tushunchalarni texnik va texnik bo'lmagan auditoriyaga samarali etkazish qobiliyatiga ega.",
         Ability_rus: "Навыки общения: Обладает отличными устными и письменными коммуникативными навыками, развиваемыми посредством совместной курсовой работы и групповых проектов. Способность эффективно доносить технические концепции как до технической, так и до нетехнической аудитории."
     },
 ]
   export default mockDataProfile;
 
 
-//   export const mockDataProfile = [
-//     {
-//         Ability: "Technical Proficiency: Well-versed in JavaScript, HTML, and CSS, with a focus on the React JS framework. Completed practical exercises and projects, demonstrating the ability to translate theoretical knowledge into tangible results."
-//     },
-//     {
-//         Ability: "Problem-Solving Skills: Developed strong analytical and problem-solving skills through various coding challenges and real-world application of programming concepts. Excels at troubleshooting and finding creative solutions."
-//     },
-//     {
-//         Ability: "Adaptability and Eagerness to Learn: Proven ability to quickly grasp new technologies and programming languages. Demonstrated commitment to continuous learning, staying informed about the latest developments in the IT field."
-//     },
-//     {
-//         Ability: "Communication Skills: Possesses excellent verbal and written communication skills, cultivated through collaborative coursework and group projects. Able to effectively convey technical concepts to both technical and non-technical audiences."
-//     },
-// ]
