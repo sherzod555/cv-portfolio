cf60944969e8e1a6a4778d476c86eb5bd3ab5c74
sherzod555 <wiut00007584@gmail.com>
2024-05-31 01:43:23 +0500

    portfolio added

---
+++ src/components/portfolio/mockPort.tsx
@@ -0,0 +1,66 @@
+import ChatGPT from "../../assets/gpt.png";
+import YouTube from "../../assets/youtube.png";
+import Emarket from "../../assets/emarket.png";
+import Finsweet from "../../assets/finsweet.png";
+import Tocha from "../../assets/tocha.png";
+import IceCream from "../../assets/icecream.png";
+import Shayna from "../../assets/shayna.png";
+
+
+
+
+export const mockDataPort = [
+
+  {
+    PortName: "ChatGPT Clone",
+    PortImg: ChatGPT,
+    PortLink: "https://chatgpt-by-sher555.netlify.app",
+    PortDesc: "Vue.JS, Next.JS, JS, TS, TailwindCss, Responsive",
+  },
+
+  {
+    PortName: "YouTube Clone",
+    PortImg: YouTube,
+    PortLink: "fake-youtube-by-sher555.netlify.app",
+    PortDesc: "Next.JS, JS, TailwindCss, Responsive",
+  },
+
+  {
+    PortName: "FinSweet",
+    PortImg: Finsweet,
+    PortLink: "https://marvelous-paprenjak-65c68a.netlify.app",
+    PortDesc: "Next.JS, JS, Scss, Responsive",
+  },
+
+  {
+    PortName: "e-Market",
+    PortImg: Emarket,
+    PortLink: "https://emarket-template-by-sher555.netlify.app",
+    PortDesc: "JavaScript, Css",
+  },
+
+  {
+    PortName: "Tocha",
+    PortImg: Tocha,
+    PortLink: "https://tocha-by-sher555.netlify.app",
+    PortDesc: "HTML, CSS, Responsive",
+  },
+
+  {
+    PortName: "Shayna",
+    PortImg: Shayna,
+    PortLink: "https://shayna-by-sher555.netlify.app",
+    PortDesc: "HTML, CSS",
+  },
+
+  {
+    PortName: "Polar Ice-Cream",
+    PortImg: IceCream,
+    PortLink: "https://polar-icecream-sherzod.netlify.app",
+    PortDesc: "HTML, CSS",
+  },
+
+  
+];
+
+export default mockDataPort;
--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,25 +1,54 @@
 import { useTheme } from "../../ThemeContext.tsx";
-import { useLanguage } from '../../LanguageContext.tsx';
+import { useLanguage } from "../../LanguageContext.tsx";
+import mockDataPort from "./mockPort.tsx";
 
 export const Portfolio = () => {
   const { isDarkMode } = useTheme();
   const { language } = useLanguage();
 
-
   return (
     <>
       <main
-        className={`${isDarkMode ? "bg-slate-700 text-white" : "bg-blue-100"}`}
-        id="portfolio"
-      >
+        className={`${isDarkMode ? "bg-slate-700 text-white" : "bg-blue-100"}`} id="portfolio">
+        
         <div className="container mx-auto xl:w-[1200px] px-7">
+        
           <div className="py-8">
-          <h2 className="text-3xl font-bold">{`${language === "en" ? "Portfolio" : language === "uz" ? "Portfolio" : "Портфолио"}`}</h2>
+        
+            <h2 className="text-3xl font-bold">{`${language === "en" ? "Portfolio" : language === "uz" ? "Portfolio" : "Портфолио"}`}</h2>
+        
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-7 pt-7">
+        
+              {mockDataPort.map((data, index) => (
+        
+                <div key={index} className="flex flex-col text-base md:text-xl">
+                
+                  <a href={data.PortLink} target="_blank" className="bg-white rounded-2xl hover:shadow-xl duration-500">
+                
+                    <div>
+                
+                      <img className="rounded-t-2xl border-b" src={data.PortImg} alt={data.PortName} />
+                
+                      <div className="hidden bg-blue-400/50">
+                        <p>{data.PortDesc}</p>
+                      </div>
+                
+                    </div>
 
-
+                    <p className="p-3 text-center">{data.PortName}</p>
+                
+                  </a>
+                
+                </div>
+              
+              ))}
             
+            </div>
+          
           </div>
+        
         </div>
+      
       </main>
     </>
   );
